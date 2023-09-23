1c5826282664cb40acc856c06585c024fe10a8b5
worladev <confidenceworlasi@gmail.com>
2023-09-23 21:46:40 +0000

    pre-session 1 and 2 discussion and task

--- pre-session1and2.py
+++ pre-session1and2.py
@@ -3,5 +3,14 @@ Pre Session 1 ==> Introductory lecture and discussion
 Mentor and Facilitator ==> ANDY AFFUL
 
 Pre Session 2
+WEEK 2 PRE-SESSION ASSIGNMENT (16th July to 22nd July)
+You are given a function that calculates the average score for a list of student scores. However, the
+function has several issues and could benefit from code refactoring to improve readability and maintainability.
+Your task is use your knowledge from the just ended Coding Without Borders programming class fix these issue.
 
+Once you have completed the task, test the function with different inputs to ensure it produces the expected results.
+Make sure to provide an explanation of the changes you made and how they address the bugs and code quality improvements.
+
+This task will help you practice debugging, code refactoring, and improving code quality by addressing common issues
+and applying best practices.
 '''
