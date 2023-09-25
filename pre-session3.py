9aefcca69627b95c88070f5dc7948f3c66ad8965
worladev <confidenceworlasi@gmail.com>
2023-09-25 06:34:22 +0000

    pre-session week 3 task

---
+++ pre-session3.py
@@ -0,0 +1,17 @@
+'''
+Pre Session 3 ==> Code Refactoring and Debugging
+Mentor and Facilitator ==> ANDY AFFUL
+
+WEEK 3 PRE-SESSION ASSIGNMENT (23rd July to 29th July)
+You are given a function that converts a decimal number(base-10) to hexadecimal(base-16). However,
+the function has several issues and could benefit from code refactoring to improve readability and
+maintainability. Your task is use your knowledge from the just ended Coding Without Borders programming
+class as well the example from last fix these issue.
+
+Once you have completed the task, test the function with different inputs to ensure it produces the
+expected results. Make sure to provide an explanation of the changes you made and how they address
+the bugs and code quality improvements.
+
+This task will help you practice debugging, code refactoring, and improving code quality by addressing
+common issues and applying best practices.
+'''
