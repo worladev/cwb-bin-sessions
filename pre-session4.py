c0754cdb7efdad222b508df0889d301ecd9b37ee
worladev <confidenceworlasi@gmail.com>
2023-09-27 18:55:17 +0000

    solution to task 2

--- pre-session4.py
+++ pre-session4.py
@@ -24,10 +24,38 @@ if sun == 'yes':
 else:
     it_is_sun = False
 
-#display message based on input received
+#display message based on value of input received
 if it_is_sat or it_is_sun:
     print("Let's have fun!")
 else:
     print("Weekdays are for work.")
+
+
+'''
+Scenario 2: Ticket Purchase
+Write a Python program that takes two inputs from the user:
+"Is the customer a student?" (student) and "Is the customer a senior citizen?" (senior).
+If the customer is a student OR a senior citizen, the program should display "You are eligible for a
+discount!" Otherwise, it should display "No discount available."
+'''
+#accept input one and check if it is 'yes' or 'no'
+customer_type = input("Is the customer a student? (enter yes/no): ")
+if customer_type == 'yes':
+    is_a_student = True
+else:
+    is_a_student = False
+
+#accept input two and check if it is 'yes' or 'no'
+customer_type2 = input("Is the customer a senior citizen? (enter yes/no): ")
+if customer_type2 == 'yes':
+    is_a_senior_citizen = True
+else:
+    is_a_senior_citizen = False
+
+#display message based on value of input received
+if is_a_student or is_a_senior_citizen:
+    print("You are eligible for a discount!.")
+else:
+    print("No discount available.")
 
 
