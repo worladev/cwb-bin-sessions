25f158ab504526820cad5376f0c3e760976aa5f1
worladev <confidenceworlasi@gmail.com>
2023-09-27 15:09:01 +0000

    solution in progress for pre-session 4 task

--- pre-session4.py
+++ pre-session4.py
@@ -4,9 +4,19 @@ Mentor and Facilitator ==> ANDY AFFUL
 
 WEEK 4 PRE-SESSION ASSIGNMENT (30th July to 5th August)
 
-Excercise Scenario: Weekend Plans
+Scenario 1: Weekend Plans
 Write a Python program that takes two inputs from the user:
 "Is it Saturday?" (sat) and "Is it Sunday?" (sun). If either of these inputs is
 true, the program should display "Let's have fun!" Otherwise, it should display
 "Weekdays are for work."
 '''
+#receive first input and check if it is 'yes' or 'no'
+sat = input("Is it Saturday? (enter yes/no): ")
+if sat == 'yes':
+    it_is_sat = True
+else:
+    it_is_sat = False
+    
+
+
+
