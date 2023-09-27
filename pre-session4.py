767a6bbd0795559427cf5564d2cc270f46417354
worladev <confidenceworlasi@gmail.com>
2023-09-27 14:53:44 +0000

    pre-session 4 task on loops and connectives

---
+++ pre-session4.py
@@ -0,0 +1,12 @@
+'''
+Pre Session 3 ==> CONNECTIVES AND LOOPS
+Mentor and Facilitator ==> ANDY AFFUL
+
+WEEK 4 PRE-SESSION ASSIGNMENT (30th July to 5th August)
+
+Excercise Scenario: Weekend Plans
+Write a Python program that takes two inputs from the user:
+"Is it Saturday?" (sat) and "Is it Sunday?" (sun). If either of these inputs is
+true, the program should display "Let's have fun!" Otherwise, it should display
+"Weekdays are for work."
+'''
