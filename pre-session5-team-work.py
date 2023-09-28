84a7c18fb7b1f380f2b3e075ea080b9d42f5cb10
worladev <confidenceworlasi@gmail.com>
2023-09-28 22:07:35 +0000

    comment added to initial refactored code

--- pre-session5-team-work.py
+++ pre-session5-team-work.py
@@ -4,18 +4,21 @@ Password Validation Task - Code Refactoring for pre-session 4
 import string
 
 
-is_not_valid_password = True
+invalid_password = True
 
-while (is_not_valid_password):
-    
+#while loop to check if all requirements for the password has been met
+while (invalid_password):
+    #accept password from user
     password = input("Create a password of at least 8 character:")
-
+    
+    #variables to check password requirements
     lower_case = 0
     upper_case = 0
     numbers = 0
     special_characters = 0
     length_of_password = len(password)
-
+    
+    #for loop to check each character of the password entered by user.
     for char in password:
         if char in string.ascii_lowercase:
             lower_case += 1
