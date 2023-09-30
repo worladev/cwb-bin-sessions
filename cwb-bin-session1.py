520279bd1b11a0cf5eeb36b56936bcb977d0929f
worladev <confidenceworlasi@gmail.com>
2023-09-30 21:52:36 +0000

    session 1 task

---
+++ cwb-bin-session1.py
@@ -0,0 +1,13 @@
+'''
+SESSION 1 (07/08/2023)
+Mentor and Facilitator ==> ANDY AFFUL
+
+Write a program that given list of words and a positive integer value 'n', finds the words that are longer than n from a given list of words.
+ - ALGORITHM
+- Create an empty list, l
+- Loop through the list
+- Get the len of each word, s and compare to 'n'
+- Add s if len > n
+- Return list l
+'''
+
