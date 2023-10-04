1278448c3ce42899fd70d17733acd80b4478ab64
worladev <confidenceworlasi@gmail.com>
2023-10-04 18:53:35 +0000

    problem statement for session 3 task

---
+++ cwb-bin-session3.py
@@ -0,0 +1,26 @@
+'''
+SESSION 2 (09/08/2023)
+Mentor and Facilitator ==> ANDY AFFUL
+
+Title: Custom List Counter - Implement Your Own count() Function
+
+PROBLEM STATEMENT: As part of your journey to become proficient Python programmers, you have been given a
+challenge to implement your own version of the count() function for lists.
+This function should count the occurrences of a specified element in a given list, just like the built-in
+count() method in Python. Your task is to write a Python function custom_count() that replicates the
+behavior of the count() method.
+
+Instructions:
+Implement a Python function custom_count(lst, element) that takes a list lst and an element element as arguments and
+returns the count of how many times the element appears in the list.
+
+# Test the custom_count function
+             0         1         2       3         4       5
+fruits = ['apple', 'banana', 'orange', 'apple', 'grape', 'apple']
+apple_count = custom_count(fruits, 'apple')
+print("Number of apples:", apple_count)
+
+numbers = ['3', '7', '2', '3', '1', '1', '3', '3', '7']
+three_count = custom_count(number, '3')
+print("Number of threes:", three_count)
+'''
