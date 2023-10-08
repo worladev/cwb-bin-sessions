4b25f662cef548109f10ffe9e63c3c768c43be04
worladev <confidenceworlasi@gmail.com>
2023-10-08 20:10:36 +0000

    problem statement for session 4 in session coding challenge

---
+++ cwb-bin-session4-ISCC.py
@@ -0,0 +1,29 @@
+'''
+SESSION 4 IN SESSION CODING CHALLENGE(16/08/2023)
+Mentor and Facilitator ==> ANDY AFFUL
+
+Title: Duplicate Letter Detector
+
+PROBLEM STATEMENT: You are developing a spelling checker application that needs to identify the first letter
+in a given word that appears twice. Your task is to write a Python function that takes a string containing
+lowercase English letters and returns the first letter that appears twice in the word.
+
+INSTRUCTION: Implement a Python function find_first_duplicate(s) that takes the following argument:
+
+s: A string consisting of lowercase English letters.
+The function should return the first letter that appears twice in the string s. If no letter appears twice,
+return None.
+
+Example:
+word1 = "mississippi"
+result1 = find_first_duplicate(word1)
+print(result1)  # Output: 's'
+
+word2 = "ghana"
+result2 = find_first_duplicate(word2)
+print(result2)  # Output: 'a'
+
+word3 = "clean"
+result3 = find_first_duplicate(word3)
+print(result3)  # Output: 'None'
+'''
