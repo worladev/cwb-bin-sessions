4980cd5ce13c00e3d57dee76192c7264302534d2
worladev <confidenceworlasi@gmail.com>
2023-10-12 14:38:24 +0000

    problem statement for session 5 task

---
+++ cwb-bin-session5.py
@@ -0,0 +1,8 @@
+'''
+SESSION 5 (21/08/2023)
+Mentor and Facilitator ==> ANDY AFFUL
+
+PROBLEM STATEMENT: Write a function called change_case that given a string, returns a string with each upper
+case letter replaced by a lower case letter and vice-versa.
+
+'''
