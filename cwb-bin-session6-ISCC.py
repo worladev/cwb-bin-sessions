9f34c33e0841b2ba4bad3e72d501e18b1b309d47
worladev <confidenceworlasi@gmail.com>
2023-10-13 17:51:10 +0000

    problem statement for session 6 ISCC task

---
+++ cwb-bin-session6-ISCC.py
@@ -0,0 +1,15 @@
+'''
+SESSION 6 (23/08/2023)
+Mentor and Facilitator ==> ANDY AFFUL
+
+IN SESSIOIN (6) ENOCH'S CODING PROBLEM ANALYSIS
+===============================================
+PROBLEM STATEMENT: The digital root of a number n is obtained as follows: Add up the digits n to get a new
+number. Add up the digits of that to get another new number.
+Keep doing this until you get a number that has only one digit. That number is the digital root.For example,
+if n = 45893, we add up the digits to get 4+5+8+9+3 = 29. We then add up the digits of 29 to get 2+9=11.
+We then add up the digits of 11 to get 1+1=2.
+Since 2 Has only one digit, 2 is our digital root. Write a function that returns the digital root of an
+integer n.
+
+'''
