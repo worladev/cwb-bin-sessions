b4211ce12be8cda90028dad58c68d83fe4bdd3d9
worladev <confidenceworlasi@gmail.com>
2023-10-16 21:26:25 +0000

    initial solution to session7 task

--- cwb-bin-session7.py
+++ cwb-bin-session7.py
@@ -1,5 +1,5 @@
 '''
-SESSION 6 (23/08/2023)
+SESSION 7 (28/08/2023)
 Mentor and Facilitator ==> ANDY AFFUL
 
 
@@ -18,3 +18,47 @@ guesses until they correctly guess the page number containing the clue. Keep tra
 it takes the player to find the correct page. Once the player correctly guesses the page number, display a
 message indicating the number of attempts it took to guess correctly.
 '''
+# Code needs refactoring.
+
+import random
+
+def guess_the_page(book_pages):
+
+  # check if list is empty
+  if len(book_pages) == 0:
+    return None
+
+  # a random number generated between 1 and book pages
+  target_page_number = random.randrange(700)
+
+  # start search using whole list
+  low = 0
+  upper = len(book_pages) - 1
+
+  count = 1  # a variable to keep count of number of guesses
+
+  while low <= upper:
+     # compute middle index and value
+     mid = low + (upper - low) // 2
+
+     if mid == target_page_number:
+
+        # ask user to guess target value
+        player_guess = int(input("Guess the page number: "))
+
+        if player_guess == target_page_number:  # value found
+           print(f"Correct!\n Total guess: {count}")
+           break
+        elif player_guess > target_page_number: # if user guess number is greater than target value
+           print("Too High.")
+        else:    # if user guess number is less than target value
+           print("Too Low.")
+        count += 1 # add 1 to count value
+
+     elif mid < target_page_number:
+        low = mid + 1
+     elif mid > target_page_number:
+        upper = mid - 1
+
+  return target_page_number
+
