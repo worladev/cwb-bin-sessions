3b4e77c32b4e76f39c75edc2d4171d36849991b1
worladev <confidenceworlasi@gmail.com>
2023-10-17 23:45:07 +0000

    2.FACTORIAL CALCULATOR code search

--- cwb-bin-session9-pre.py
+++ cwb-bin-session9-pre.py
@@ -43,3 +43,29 @@ def palindromes(word):
     print(word, " is palindrome.")
 
 palindromes("madam")
+
+
+'''
+3. FACTORIAL CALCULATOR
+
+Find code that calculates the factorial of a given number.
+
+REF: https://www.programiz.com/python-programming/examples/factorial
+'''
+#Using loops
+# Python program to find the factorial of a number provided by the user.
+
+def calculateFactorial(num):
+  factorial = 1
+  #check if the number is negative, positive or zero
+  if num < 0:
+    print("Sorry, factorial does not exist for negative numbers")
+  elif num == 0:
+    print("The factorial of 0 is 1")
+  else:
+    for i in range(1, num + 1):
+       factorial = factorial * i
+  return factorial
+
+
+print(f"The factorial of 7 is {calculateFactorial(7)}")
