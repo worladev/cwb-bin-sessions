46a2190165037a01420d216899a0daa6e33682df
worladev <confidenceworlasi@gmail.com>
2023-10-20 13:52:58 +0000

    search outcome for calculator app

--- cwb-bin-session9-pre.py
+++ cwb-bin-session9-pre.py
@@ -180,3 +180,60 @@ def bubble_sort(array):
 
     return array
   
+
+'''
+8. CALCULATOR APP
+
+Discover code for a simple calculator application that can perform basic operations like addition, subtraction, multiplication, and division.
+
+REF: https://www.digitalocean.com/community/tutorials/how-to-make-a-calculator-program-in-python-3
+'''
+def calculate():
+    operation = input('''
+Please type in the math operation you would like to complete:
++ for addition
+- for subtraction
+* for multiplication
+/ for division
+''')
+
+    number_1 = int(input('Please enter the first number: '))
+    number_2 = int(input('Please enter the second number: '))
+
+    if operation == '+':
+        print('{} + {} = '.format(number_1, number_2))
+        print(number_1 + number_2)
+
+    elif operation == '-':
+        print('{} - {} = '.format(number_1, number_2))
+        print(number_1 - number_2)
+
+    elif operation == '*':
+        print('{} * {} = '.format(number_1, number_2))
+        print(number_1 * number_2)
+
+    elif operation == '/':
+        print('{} / {} = '.format(number_1, number_2))
+        print(number_1 / number_2)
+
+    else:
+        print('You have not typed a valid operator, please run the program again.')
+
+    # Add again() function to calculate() function
+    again()
+
+def again():
+    calc_again = input('''
+Do you want to calculate again?
+Please type Y for YES or N for NO.
+''')
+
+    if calc_again.upper() == 'Y':
+        calculate()
+    elif calc_again.upper() == 'N':
+        print('See you later.')
+    else:
+        again()
+
+calculate()
+
