25d204b603c9647b5527576714525a0f60564471
worladev <confidenceworlasi@gmail.com>
2023-10-22 23:40:51 +0000

    search outcome for Currency Converter

--- cwb-bin-session9-pre.py
+++ cwb-bin-session9-pre.py
@@ -411,3 +411,48 @@ t = input("Enter the time in seconds: ")
 # function call
 countdown(int(t))
 
+
+'''
+13. CURRENCY CONVERTER
+
+Locate code for a currency converter that converts between different currencies using exchange rates.
+
+REF: https://www.learningmilestone.com/post/currency-converter-python
+'''
+#  According to the problem,the function display_menu should not return any value.It should just display the menu
+def display_menu():
+  print("1. Convert USD to Euro(EUR)")
+  print("2. Convert USD to British Pound Sterling (GBP)")
+  print("3. Convert USD to Japenese Yen(JPY)")
+  print("4. Convert USD to Canadian Dollar(CAD)")
+  print("5. Exit")
+#The other four functions that convert currencies accept a value through an argument and return the converted value
+def USD_to_EU(value):
+  return value*0.94
+def USD_to_GBP(value):
+  return value*0.79
+def USD_to_JPY(value):
+  return value*113
+def USD_to_CAD(value):
+  return value*1.33
+while True:
+  display_menu()
+  choice=int(input())
+  if choice==5:
+    print("Bye!")
+    break
+  else:
+    amount=float(input("Enter an amount in US dollars: "))
+    if choice==1:
+      print(amount,"USD",USD_to_EU(amount),"Euro")
+    elif choice==2:
+      print(amount,"USD",USD_to_GBP(amount),"GBP")
+    elif choice==3:
+      print(amount,"USD",USD_to_JPY(amount),"JPY")
+    elif choice==4:
+      print(amount,"USD",USD_to_CAD(amount),"CAD")
+
+
+'''
+
+'''
