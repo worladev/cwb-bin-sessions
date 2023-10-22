9b7c23d49493dcc6ca6656ccf76b3cb363a06d3a
worladev <confidenceworlasi@gmail.com>
2023-10-22 22:42:40 +0000

    search outcome for Number Guessing Game

--- cwb-bin-session9-pre.py
+++ cwb-bin-session9-pre.py
@@ -312,3 +312,33 @@ Find code for a number guessing game where the program generates a random number
 
 REF: https://djangocentral.com/creating-a-guessing-game-in-python/
 '''
+import random
+
+def number_guessing_game():
+    number = random.randint(1, 10)
+    
+    #solving using binary search algorithm.
+    # lower = 0 #
+    # upper = len(number) - 1 #
+
+    player_name = input("Hello, What's your name? ")
+    number_of_guesses = 0
+
+    print("Okay, " + player_name + "! I am guessing a number between 1 and 10:")
+
+    while number_of_guesses < 5:
+        guess = int(input())
+        number_of_guesses += 1
+
+        if guess < number:
+            print('Your guess is too low.')
+        elif guess > number:
+            print('Your guess is too high.')
+        else:
+            break
+
+    if guess == number:
+        print('Congratulations, ' + player_name + '! You guessed the number in ' + str(number_of_guesses) + ' tries!')
+    else:
+        print('Sorry, ' + player_name + ', you did not guess the number. The number was ' + str(number))
+        
