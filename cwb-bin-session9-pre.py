2c8919570f7b22006e68c25f1d806595743dc9a4
worladev <confidenceworlasi@gmail.com>
2023-10-22 23:50:03 +0000

    search outcome for The Power of Two

--- cwb-bin-session9-pre.py
+++ cwb-bin-session9-pre.py
@@ -454,5 +454,38 @@ while True:
 
 
 '''
+14. POWER OF TWO
 
+Find code that determines if a number is a power of 2.
+
+REF: geeksforgeeks.org/program-to-find-whether-a-given-number-is-power-of-2/
 '''
+# Python program to check if given
+# number is power of 2 or not
+
+# Function to check if x is power of 2
+def isPowerOfTwo(n):
+	if (n == 0):
+		return False
+	while (n != 1):
+		if (n % 2 != 0):
+			return False
+		n = n // 2
+
+	return True
+#n -> n &  (n -1) > 0
+
+# Driver code
+if __name__ == "__main__":
+
+	# Function call
+	if(isPowerOfTwo(31)):
+		print('Yes')
+	else:
+		print('No')
+	if(isPowerOfTwo(64)):
+		print('Yes')
+	else:
+		print('No')
+
+# This code is contributed by Danish Raza
