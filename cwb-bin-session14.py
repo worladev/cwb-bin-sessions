4a67d65d427668c0b8c4e8f171e002e6750a6f1e
worladev <confidenceworlasi@gmail.com>
2023-10-31 22:55:22 +0000

    problem statement for session14 task (acknowledgement checker)

---
+++ cwb-bin-session14.py
@@ -0,0 +1,33 @@
+'''
+SESSION 14 (/09/2023)
+Mentor and Facilitator ==> ANDY AFFUL
+
+PROBLEM STATEMENT: You are tasked with simulating the data acknowledgment process the the Transport Layer which
+is responsible for ensuring reliable data transfer between two devices. It uses acknowledgment packets to confirm
+the successful reception of data.
+
+Write a Python function acknowledgment_checker(sent_packets, received_acknowledgments) that takes the following
+arguments:
+
+INPUT
+sent_packets: A list of integers representing unique packet IDs sent from the sender device.
+received_acknowledgments: A list of integers representing unique acknowledgment packet IDs received by the sender
+device.
+
+OUTPUT: The function should return a list of integers, indicating the IDs of packets that have not been
+successfully acknowledged.
+
+Example:
+sent_packets = [1, 2, 3, 4, 5]
+received_acknowledgments = [2, 4]
+
+missing_packets = acknowledgment_checker(sent_packets, received_acknowledgments)
+print(missing_packets)
+# Output: [1, 3, 5]
+
+Explanation:
+In this question, the acknowledgment_checker function simulates the acknowledgment process of the Transport Layer.
+It takes two lists as input: sent_packets representing unique packet IDs sent and received_acknowledgments
+representing unique acknowledgment packet IDs received. The function calculates and returns the IDs of packets
+that have not been successfully acknowledged.
+'''
