1a777d9324c05500a4263ac7f0d2ad377c2b28cf
worladev <confidenceworlasi@gmail.com>
2023-11-01 22:12:49 +0000

    problem statement for session15-ISCC (pangram detector)

---
+++ cwb-bin-session15-ISCC.py
@@ -0,0 +1,22 @@
+'''
+SESSION 15 (/09/2023)
+Mentor and Facilitator ==> ANDY AFFUL
+
+TITLE: "Pangram Checker"
+
+PROBLEM STATEMENT: A pangram is a sentence that contains every letter of the alphabet at least once. Your task
+is to write a Python function is_pangram(sentence) that checks if a given sentence is a pangram or not.
+
+OUTPUT: The function should return True if the sentence is a pangram and False otherwise.
+
+Example:
+sentence1 = "The quick brown fox jumps over the lazy dog"
+result1 = is_pangram(sentence1)
+print(result1)
+# Output: True
+
+sentence2 = "Hello, World!"
+result2 = is_pangram(sentence2)
+print(result2)
+# Output: False
+'''
