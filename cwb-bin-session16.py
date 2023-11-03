dd5aed72cd4c6e3bb3ef38bf6be54a322ae5542c
worladev <confidenceworlasi@gmail.com>
2023-11-03 19:04:08 +0000

    problem statement for session16 task (calculate fuel cost)

---
+++ cwb-bin-session16.py
@@ -0,0 +1,53 @@
+'''
+SESSION 16 (02/10/2023)
+Mentor and Facilitator ==> ANDY AFFUL
+
+TITLE: Delivery Route Fuel Cost Calculation
+
+PROBLEM STATEMENT: A delivery driver starts at a given starting point and needs to reach an ending point.
+There are several intermediate points listed in a sequence.
+The driver must visit these intermediate points in the given order. Your task is to calculate the total cost
+of fuel used for the trip, considering the cost per kilometer.
+
+Write a function
+calculate_fuel_cost(start: Tuple[int, int], points: List[Tuple[int, int]], cost_per_km: float) -> float to solve this problem.
+
+Function Signature:
+def calculate_fuel_cost(start: Tuple[int, int], points: List[Tuple[int, int]], cost_per_km: float) -> float:
+    pass
+
+INPUT:
+start: A tuple representing the starting point as (x, y) coordinates.
+points: A list of tuples representing intermediate points. Each tuple is a (x, y) coordinate.
+cost_per_km: A float representing the cost per kilometer of fuel.
+
+OUTPUT: Returns a float representing the total cost of fuel used for the trip.
+
+NOTE: Distance Calculation Formula:
+The distance between two points (x1,y1) and (x2,y2) can be calculated using the Euclidean distance formula:
+
+Example 1:
+
+start = (0, 0)
+points = [(1, 1), (2, 2), (3, 3), (4, 4)]
+cost_per_km = 2.5  # $2.5 per kilometer
+
+total_cost = calculate_fuel_cost(start, points, cost_per_km)
+# Output: 14.14  # Assuming Euclidean distances between points
+#Distance from (0, 0) to (1, 1): ≈1.41 (rounded to 2 decimal places)
+#Distance from (1, 1) to (2, 2): ≈1.41 (rounded to 2 decimal places)
+#Distance from (2, 2) to (3, 3): ≈1.41 (rounded to 2 decimal places)
+#Distance from (3, 3) to (4, 4): ≈1.41 (rounded to 2 decimal places)
+#Total distance traveled: 1.41 + 1.41 + 1.41 + 1.41 = 5.64 (rounded to 2 decimal places)
+1.41+1.41+1.41+1.41=5.64 (rounded to 2 decimal places)
+
+Therefore, the total cost is
+5.64 * 2.5 =  14.10 (rounded to 2 decimal places).
+
+Example 2:
+start = (2, 4)
+points = [(2,0),(-1,2),(-3,3},(2,7),(4,-4)]
+cost_per_km = 3.3  # $3.3 per kilometer
+total_cost = calculate_fuel_cost(start, points, cost_per_km)
+#total_cost = $90.5
+'''
