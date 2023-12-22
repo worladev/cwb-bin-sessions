6937ee7237d076e738dc469d2f1de724e2bb11f3
worladev <confidenceworlasi@gmail.com>
2023-12-22 23:04:33 +0000

    problem statement for session21 ISCC (peak oxygen level)

---
+++ cwb-bin-session21-ISCC.py
@@ -0,0 +1,47 @@
+'''
+IN SESSION 21 CODING CHALLENGE (23/10/2023)
+Mentor and Facilitator ==> ANDY AFFUL
+
+TITLE: Peak Oxygen Level in a Training Session
+
+PROBLEM STATEMENT: Alice is an athlete training to increase her blood oxygen level during a workout session.
+She records her blood oxygen levels at different time intervals and organizes them into an array oxygen_levels
+where oxygen_levels[i] represents her blood oxygen level at time i. The training session follows the pattern
+
+Initially, Alice's blood oxygen level is low and gradually increases. At a certain point, her oxygen level
+reaches a peak. After the peak, it starts decreasing. Alice wants to find out the time when she reached her
+peak oxygen level.
+
+Write a function find_peak_time(oxygen_levels: List[int]) -> int to help her determine the index corresponding to her peak oxygen level.
+
+INPUT:
+oxygen_levels: A list of integers [oxygen_levels[0], oxygen_levels[1], ..., oxygen_levels[n-1]] where 1 ≤ n ≤ 10^6 and
+80 ≤ oxygen_levels[i] ≤ 100. The values represent Alice's blood oxygen levels during the training session.
+
+OUTPUT:
+Return an integer representing the time index when Alice reached her peak oxygen level.
+
+Example:
+oxygen_levels = [85, 88, 92, 95, 97, 96, 94, 91, 89, 86, 84]
+find_peak_time(oxygen_levels)
+
+Output: 4
+Explanation:
+In the given example, Alice's peak oxygen level is 97 which occurs at index 4 in the oxygen_levels array.
+
+3 Approaches
+-------------
+- Use the in built max() method to find the max element
+- Use normal linear search where we inspect from the 2nd to last but 1 element
+   85, 88, 92, 95, 97, 96, 94, 91, 89, 86, 84
+   = currentNumber, previousNumber, nextNumber
+   peakNumber =>  previous < currentNumber > nextNumber
+- Use binary search
+    mid = low + (high-low)/2
+    peak = list[mid - 1] < list[mid] > list[mid + 1]
+     
+    if(list[mid+1] < list[mid]):
+        upper = mid - 1
+    else:
+        lower = mid + 1
+'''
